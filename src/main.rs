216c69a9acbcaead7643d8aa415849a45cd6ce40
Blaine Traudt <blaine-t@bathost.net>
2025-03-25 20:38:40 -0500

    Switch db to redb and add boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-use link::*;
+use redb::{Database, Error, TableDefinition};
 
-fn main() {
-    let env = create_env();
-    let db = create_db(&env);
-    let rtxn = create_rtxn(&env);
-    
-    let key = "traudtDev";
+const TABLE: TableDefinition<&str, &str> = TableDefinition::new("my_data");
 
-    let mut i: u128 = 0;
-    loop {
-        if let Err(e) = add_kvp(db, &env, &i.to_string(), &i.to_string()) {
-            eprintln!("Error adding key-value pair: {}", e);
-            break;
-        }
-        i += 1;
-        println!("{:?}", get_kvp(db, &rtxn, &i.to_string()));
+fn main() -> Result<(), Error> {
+    let db = Database::create("db.redb")?;
+    let write_txn = db.begin_write()?;
+    {
+        let mut table = write_txn.open_table(TABLE)?;
+        table.insert("my_key", "123")?;
     }
-    println!("{:?}", get_kvp(db, &rtxn, key));
+    write_txn.commit()?;
+
+    let read_txn = db.begin_read()?;
+    let table = read_txn.open_table(TABLE)?;
+    assert_eq!(table.get("my_key")?.unwrap().value(), "123");
+
+    Ok(())
 }
