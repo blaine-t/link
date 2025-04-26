c93567e6e2a3cfb53e5f154ec8c7251d2557ba2f
blaine-t <blaine-t@bathost.net>
2025-04-26 14:24:55 -0500

    Unwrapped serve

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
 use actix_files as fs;
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder};
 use redb::{Database, Error, TableDefinition};
 
 const TABLE: TableDefinition<&str, &str> = TableDefinition::new("my_data");
 
 #[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
+async fn serve_link(key: web::Path<String>, db: web::Data<Database>) -> impl Responder {
+    let read_txn = db.begin_read().unwrap();
+    let table = read_txn.open_table(TABLE).unwrap();
+    format!("Hello {}!", table.get(key.as_str()).unwrap().unwrap().value())
 }
 
-fn db() -> Result<(), Error> {
-    let db = Database::create("db.redb")?;
-    let write_txn = db.begin_write()?;
-    {
-        let mut table = write_txn.open_table(TABLE)?;
-        table.insert("my_key", "123")?;
-    }
-    write_txn.commit()?;
-
-    let read_txn = db.begin_read()?;
-    let table = read_txn.open_table(TABLE)?;
-    assert_eq!(table.get("my_key")?.unwrap().value(), "123");
-    println!("DB written to and accessed");
-
-    Ok(())
+#[post("/{name}")]
+async fn create_link(name: web::Path<String>, db: web::Data<Database>) -> impl Responder {
+    format!("Hello {}!", &name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let _ = db();
-    HttpServer::new(|| App::new().service(hello).service(fs::Files::new("/", "./public")))
+    let db = web::Data::new(Database::create("db.redb").unwrap());
+    HttpServer::new(move || App::new().app_data(db.clone()).service(serve_link).service(create_link).service(fs::Files::new("/", "./public")))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
